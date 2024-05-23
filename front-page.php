48ef240c8c9dc8cd24616a72f276e7717ac81cf0
Jess Rodier <jrodier@wilsonlanguage.com>
2024-05-23 08:57:50 -0400

    finish frontpage

--- front-page.php
+++ front-page.php
@@ -77,23 +77,60 @@
       <h2 class="title">REAC is what we do.</h2>
       <p class="mt-2">The <em>only</em> choice of the <a href="#TODO">US Housing Consultants</a></p>
       <p class="mt-5">The REAC Company specializes in affordable housing preparation repair work for all HUD REAC and state housing inspections. We can assist with all your manpower needs to alleviate backlogged work orders, fill labor shortages, or assist generally with property physical compliance and stabilization.</p>
-      <p class="mt-3">Affordable housing properties can face tremendous challenges preparing for inspections and working out of non-compliance issues, and often, properties do not have the on-hand labor resources required. The REAC Company can assist with mobilizing large or small crews on short notice while providing quality work.</p>
-      <p class="mt-3">We have a deep understanding of REAC’s scoring methodology and take our commitment to quality very seriously to ensure that repairs made are meeting industry standard requirements according to REAC’s guidance.</p>
+      <p>Affordable housing properties can face tremendous challenges preparing for inspections and working out of non-compliance issues, and often, properties do not have the on-hand labor resources required. The REAC Company can assist with mobilizing large or small crews on short notice while providing quality work.</p>
+      <p>We have a deep understanding of REAC’s scoring methodology and take our commitment to quality very seriously to ensure that repairs made are meeting industry standard requirements according to REAC’s guidance.</p>
     </div>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/home-section-triangle.svg" alt="section bottom" aria-hidden="true">
   </section>
+
+  <img class="bottom-tri" src="<?php echo get_template_directory_uri(); ?>/assets/home-section-triangle.svg" alt="section bottom" aria-hidden="true">
 
   <!-- What is NSPIRE? / TODO Section Type?? -->
   <section id="what-is-nspire" class="container-fluid full dark-bg">
+    <div class="container row">
+      <div class="col-6">
+        <p class="lead primary">REAC</p>
+        <h2 class="title">What is REAC?</h2>
+        <p class="mt-3">Real Estate Assessment Center (REAC), a division of HUD with the mission to provide customers with independent, actionable assessments that advance risk-informed decisions about the condition of the nation’s affordable housing portfolio.</p>
+      </div>
+      <div class="col-6">
+        <img class="graphic ms-auto" src="<?php echo get_template_directory_uri(); ?>/assets/hero-bg.PNG" alt="REAC Photo" aria-hidden="true">
+      </div>
+    </div>
+  </section>
+
+  <!-- Something Else / TODO Section Type?? -->
+  <section id="more-info" class="container-fluid full dark-bg">
+    <div class="container row">
+      <div class="col-6">
+        <img class="graphic me-auto" src="<?php echo get_template_directory_uri(); ?>/assets/hero-bg.PNG" alt="NSPIRE Photo" aria-hidden="true">
+      </div>
+      <div class="col-6">
+        <p class="lead primary">NSPIRE</p>
+        <h2 class="title">What is NSPIRE?</h2>
+        <p class="mt-3">National Standards for the Physical Inspection of Real Estate (NSPIRE), the new inspection model implemented for most properties on 10/1/23, prioritizes health, safety, and functional defects over appearance. It implements inspections that better reflect the true physical conditions of the property. The NSPIRE model supports the adoption of sound, year-round maintenance practices.</p>
+      </div>
+    </div>
+  </section>
+
+  <!-- Badges -->
+  <section id="badges" class="container-fluid">
+    <div class="container row">
+      <div class="col-4"><img class="badge" src="<?php echo get_template_directory_uri(); ?>/assets/badge-trusted.svg" alt="trusted badge"></div>
+      <div class="col-4"><img class="badge" src="<?php echo get_template_directory_uri(); ?>/assets/badge-guaranteed.svg" alt="guaranteed badge"></div>
+      <div class="col-4"><img class="badge" src="<?php echo get_template_directory_uri(); ?>/assets/badge-trained.svg" alt="trained badge"></div>
+    </div>
+  </section>
+
+  <!-- Contact Us -->
+  <section id="contact-us" class="padded container-fluid">
     <div class="container">
-      <hr class="short">
-      <p class="lead primary">Our Company</p>
-      <h2 class="title">REAC is what we do.</h2>
-      <p class="mt-2">The <em>only</em> choice of the <a href="#TODO">US Housing Consultants</a></p>
-      <p class="mt-5">The REAC Company specializes in affordable housing preparation repair work for all HUD REAC and state housing inspections. We can assist with all your manpower needs to alleviate backlogged work orders, fill labor shortages, or assist generally with property physical compliance and stabilization.</p>
-      <p class="mt-3">Affordable housing properties can face tremendous challenges preparing for inspections and working out of non-compliance issues, and often, properties do not have the on-hand labor resources required. The REAC Company can assist with mobilizing large or small crews on short notice while providing quality work.</p>
-      <p class="mt-3">We have a deep understanding of REAC’s scoring methodology and take our commitment to quality very seriously to ensure that repairs made are meeting industry standard requirements according to REAC’s guidance.</p>
-</div>
+      <div class="contact-form-container">
+        <p class="lead primary">Contact Us</p>
+        <h2 class="title">Don't wait, reach out today.</h2>
+        <p>Have questions or need an estimate? We're here for you!</p>
+        <p><em>Insert form here.</em></p>
+      </div>
+    </div>
   </section>
 
   <!--  -->
